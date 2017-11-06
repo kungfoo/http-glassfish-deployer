ba26f83b75045c84c78ed230d68f175b9890ff3a
Silvio Heuberger <kung.foo@gmx.net>
2017-11-06 10:54:14 +0100

    Inject war.archivePath into run args

--- application/src/main/java/ch/mollusca/glassfish/deployer/application/HttpDeployerApplication.java
+++ application/src/main/java/ch/mollusca/glassfish/deployer/application/HttpDeployerApplication.java
@@ -7,17 +7,20 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 
+import static java.lang.System.exit;
+
 public class HttpDeployerApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HttpDeployerApplication.class);
 
     private static final String APPLICATION_NAME = "ch.mollusca.sample";
     private static final String CONTEXT_ROOT = "ch.mollusca.sample";
-    private static final String WAR_FILE = "sample-war.war";
 
-    private static final String PASSWORD = "f34ccb5c";
+    private static final String PASSWORD = "b0c81d64";
 
     public static void main(String... args) throws InterruptedException {
+        File deploymentArchive = ensureValidArguments(args);
+
         HttpDeployer deployer = new HttpDeployer.Builder()
                 .insecure()
                 .password(PASSWORD)
@@ -25,11 +28,19 @@ public class HttpDeployerApplication {
                 .contextRoot(CONTEXT_ROOT)
                 .build();
 
-        GlassfishAdministrationResult result = deployer.deploy(new File(WAR_FILE));
+        GlassfishAdministrationResult result = deployer.deploy(deploymentArchive);
         LOGGER.info("Result: {}", result);
 
         Thread.sleep(500);
         result = deployer.undeploy();
         LOGGER.info("Result: {}", result);
+    }
+
+    private static File ensureValidArguments(String[] args) {
+        if(args.length < 1) {
+            LOGGER.error("usage: HttpDeployerApplication ${path-to-deployment-archive}");
+            exit(1);
+        }
+        return new File(args[0]);
     }
 }
